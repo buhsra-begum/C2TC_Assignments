276ee62b1623749d7c06bb83009bb796052ed62c
bushy-ash <bushyansari30@gmail.com>
2025-10-02 13:27:40 +0530

    commit

---
+++ src/AssignmentMain.java
@@ -0,0 +1,21 @@
+import com.tnsif.assignment_1.employees.Manager;
+import com.tnsif.assignment_1.employees.Developer;
+import com.tnsif.assignment_1.utilities.EmployeeUtilities;
+
+public class AssignmentMain {
+    public static void main(String[] args) {
+        Manager mgr = new Manager("Alice", 101, 90000, "IT");
+        Developer dev = new Developer("Bob", 102, 70000, "Java");
+
+        System.out.println("=== Manager Details ===");
+        EmployeeUtilities.printEmployeeDetails(mgr);
+        System.out.println("Department: " + mgr.getDepartment());
+
+        System.out.println("\n=== Developer Details ===");
+        EmployeeUtilities.printEmployeeDetails(dev);
+        System.out.println("Programming Language: " + dev.getProgrammingLanguage());
+    }
+}
+
+
+	
